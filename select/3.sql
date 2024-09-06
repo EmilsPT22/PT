decf760f8c5dd2b65f633a77ca51d23af4711e38
Emils <you@example.com>
2024-09-06 10:06:21 +0300

    select where date range, not in

---
+++ select/3.sql
@@ -0,0 +1,11 @@
+--  use sql_store;
+--  select * from orders
+--  where order_date > "2017-12-31" and order_date < "2019-01-01" 
+
+-- select * from customers
+-- where state != "VA" and state != "CO"
+-- where state not in ("VA","CO","FL")
+
+
+
+
