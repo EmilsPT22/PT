7433dc98a6e1ca7df94afafc6d06f03f4e0d6019
Emils <you@example.com>
2024-09-06 10:27:02 +0300

    select with logical operators

---
+++ select/4.sql
@@ -0,0 +1,13 @@
+-- use sql_store;
+-- select * from customers
+-- where birth_date > "1990-01-01" and points > 1000;
+
+
+-- select * from customers
+-- where birth_date > "1990-01-01" or points > 1000;
+
+
+-- select * from customers
+-- where birth_date > "1990-01-01" or points > 1000 and state in ("VA","CO");
+
+
