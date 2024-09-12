b4c8fe067a19588552083892d1cdb2cefd22c970
Emils <you@example.com>
2024-09-12 08:48:47 +0300

    join order with status order

---
+++ 2select/1.sql
@@ -0,0 +1,16 @@
+-- use sql_hr;
+-- select * from employees
+
+-- use sql_store;
+-- select first_name, last_name from customers
+
+-- use sql_hr;
+-- select * from employees
+-- where reports_to is NULL
+
+-- use sql_store;
+-- select * from orders
+-- where status = 1
+
+
+ 
