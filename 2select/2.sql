47d7d1a5cc172636f393e76c6402d258732cbeac
Emils <you@example.com>
2024-09-12 10:03:35 +0300

    fix; use db name added

---
+++ 2select/2.sql
@@ -0,0 +1,33 @@
+use sql_store;
+ 
+ -- izmantojam alias
+
+-- SELECT 
+--     o.order_id, c.customer_id, c.first_name, c.last_name
+-- FROM orders o
+-- JOIN customers c ON o.customer_id = c.customer_id;
+
+
+	-- iegut to pasu rezultatu using
+
+-- SELECT 
+--     o.order_id, c.customer_id, c.first_name, c.last_name
+-- FROM orders o
+-- JOIN customers c 
+-- 		using(customer_id);
+        
+        
+	-- kreisaja puse panemt customers
+    
+    
+--     SELECT 
+-- *
+-- FROM customers c 
+-- JOIN orders o
+-- 		using(customer_id) 
+    
+    
+
+
+    
+    
