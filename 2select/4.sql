799de9a9fc3637188bdeea0029b4bcc6779fba63
Emils <you@example.com>
2024-09-12 10:25:36 +0300

    inner join, left outer join right outer join

---
+++ 2select/4.sql
@@ -0,0 +1,21 @@
+-- outer join
+
+-- orders + customers
+
+-- use sql_store;
+
+-- inner join
+-- select * from orders o
+-- join customers c
+-- using(customer_id)
+
+-- left outer join
+-- select * from orders o
+-- left join customers c
+-- using(customer_id)
+
+
+-- right outer join
+-- select * from orders o
+-- right join customers c
+-- using(customer_id)
